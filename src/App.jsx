85274d8f3b299480a5b6d4a274dad9892770640b
Luiza Petrinina <petrinina95@gmail.com>
2025-03-14 14:23:07 +0200

    Remove main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import "./App.css";
-import Header from "./components/Header/Header";
-
-import Main from "./components/Main";
+import Header from "./components/header/Header";
+import Catalog from "./components/catalog/Catalog";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <Header />
-      <Main />
+
+      <div className="bg-white pt-16">
+        <div className="mx-auto max-w-7xl py-2 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-screen-xl bg-white p-5 sm:p-10 md:p-16">
+            <Home />
+            <Catalog />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
