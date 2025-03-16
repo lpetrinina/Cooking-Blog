29ac43b538570cde4d47b600b5b769a2d0ea8fe8
Luiza Petrinina <petrinina95@gmail.com>
2025-03-16 21:24:48 +0200

    Fix font size

--- src/components/Header/Navigation.jsx
+++ src/components/Header/Navigation.jsx
@@ -1,11 +1,9 @@
 import { NavLink } from "react-router";
 
 export default function Navigation({ navigation }) {
-  console.log(navStyles);
-
   return (
     <>
-      <div className="hidden md:ml-6 md:flex md:space-x-8">
+      <div className="hidden md:ml-8 md:flex md:space-x-9">
         {navigation.map((item) => (
           <NavLink
             key={item.name}
@@ -24,7 +22,7 @@ export default function Navigation({ navigation }) {
 
 const navStyles = {
   activeStyle:
-    "inline-flex items-center border-b-2 border-pink-500 px-1 pt-1 text-sm font-medium text-gray-900",
+    "inline-flex items-center border-b-2 border-pink-500 px-1 pt-1 text-md font-medium text-gray-900",
   hoverStyle:
-    "inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-pink-300 hover:text-gray-700",
+    "inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-md font-medium text-gray-500 hover:border-pink-300 hover:text-gray-700",
 };
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -5,7 +5,7 @@ export default function Login() {
   return (
     <>
       <div className="flex flex-col justify-center bg-white py-12 sm:px-6 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="sm:mx-auto sm:w-full sm:max-w-lg">
           <div className="bg-gradient-to-t from-white to-pink-100 px-4 py-8 shadow-lg sm:rounded-lg sm:px-10">
             {/* <!-- Header --> */}
             <div className={styles["header"]}>
@@ -13,7 +13,7 @@ export default function Login() {
             </div>
 
             {/* <!-- Form --> */}
-            <form className="space-y-6">
+            <form className="space-y-7">
               {/* <!-- Email --> */}
               <div className={styles["filed"]}>
                 <input type="email" id="email" required />
@@ -32,7 +32,7 @@ export default function Login() {
             <div className={styles["link-container"]}>
               <p>Don't have an account?</p>
               <a href="#" className={styles["link-to-register"]}>
-                Register now &rarr;
+                Sign up &rarr;
               </a>
             </div>
           </div>
