a8bcef9dac54966a62ca1e8166af705516980108
Luiza Petrinina <petrinina95@gmail.com>
2025-03-21 14:04:35 +0200

    Show edit page with prepopulated values in the form

--- client/src/components/recipe-edit/RecipeEdit.jsx
+++ client/src/components/recipe-edit/RecipeEdit.jsx
@@ -1,7 +1,21 @@
+import { useNavigate, useParams } from "react-router";
+import { useEffect, useState } from "react";
+
 import PrimaryBtn from "../common/buttons/PrimaryBtn";
 import styles from "./RecipeEdit.module.css";
+import recipeService from "../../service/recipeService";
 
 export default function RecipeEdit() {
+  const navigate = useNavigate();
+  const { recipeId } = useParams();
+  const [recipe, setRecipe] = useState({});
+
+  useEffect(() => {
+    recipeService.getOne(recipeId).then((result) => setRecipe(result));
+  }, [recipeId]);
+
+  };
+
   return (
     <>
       <div className="flex flex-col justify-center bg-white sm:px-6 lg:px-8">
@@ -13,46 +27,74 @@ export default function RecipeEdit() {
             </div>
 
             {/* <!-- Form --> */}
-            <form className="space-y-6">
+            <form className="space-y-6" action={formAction}>
               {/* <!-- Title --> */}
               <div className={styles["filed"]}>
-                <input type="text" id="title" name="title" />
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  defaultValue={recipe.title}
+                />
                 <label htmlFor="title">Title</label>
               </div>
 
               {/* <!-- Description --> */}
               <div className={styles["filed"]}>
-                <textarea id="description" name="description" />
+                <textarea
+                  id="description"
+                  name="description"
+                  defaultValue={recipe.description}
+                />
                 <label htmlFor="description">Description</label>
               </div>
 
               {/* <!-- Preparation time --> */}
               <div className={styles["filed"]}>
-                <input type="text" id="prepTime" name="prepTime" />
+                <input
+                  type="text"
+                  id="prepTime"
+                  name="prepTime"
+                  defaultValue={recipe.prepTime}
+                />
                 <label htmlFor="prepTime">Preparation time (in minutes)</label>
               </div>
 
               {/* <!-- Cook time --> */}
               <div className={styles["filed"]}>
-                <input type="text" id="cookTime" name="cookTime" />
+                <input
+                  type="text"
+                  id="cookTime"
+                  name="cookTime"
+                  defaultValue={recipe.cookTime}
+                />
                 <label htmlFor="cookTime">Cook time (in minutes)</label>
               </div>
 
               {/* <!-- Image --> */}
               <div className={styles["filed"]}>
-                <input type="text" id="imageUrl" name="imageUrl" />
+                <input
+                  type="text"
+                  id="imageUrl"
+                  name="imageUrl"
+                  defaultValue={recipe.imageUrl}
+                />
                 <label htmlFor="imageUrl">Image</label>
               </div>
 
               {/* <!-- Ingredients --> */}
               <div className={styles["filed"]}>
-                <textarea id="ingredients" name="ingredients" />
+                <textarea
+                  id="ingredients"
+                  name="ingredients"
+                  defaultValue={recipe.ingredients}
+                />
                 <label htmlFor="ingredients">Ingredients</label>
               </div>
 
               {/* <!-- Steps --> */}
               <div className={styles["filed"]}>
-                <textarea id="steps" name="steps" />
+                <textarea id="steps" name="steps" defaultValue={recipe.steps} />
                 <label htmlFor="steps">Steps</label>
               </div>
 
