6bd2b2f2c526ef053f7a6acd6413b6256b8397e2
Luiza Petrinina <petrinina95@gmail.com>
2025-03-24 14:53:47 +0200

    Add new logo

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -18,16 +18,13 @@ export default function Header() {
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex h-16 justify-between">
             {/* Logo */}
-            <div className="flex items-center">
+            <div className="flex items-center gap-6">
               <Link to="/" className="flex flex-shrink-0 items-center">
                 <img
                   className="h-9 w-auto"
-                  src="/baked-cake-cup.svg"
+                  src="/logo/baked-cake-cup-svgrepo-com.png"
                   alt="Logo"
                 />
-                <span className="ml-3 text-xl font-bold text-pink-600">
-                  Healthy Sweet Life
-                </span>
               </Link>
 
               {/* Main navigation */}
