67a0223bfb33b45b3013d58c00ca3e595032a7fc
Luiza Petrinina <petrinina95@gmail.com>
2025-03-26 14:10:12 +0200

    Fix design problems

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
 
         <div className="bg-white pt-16">
           <div className="mx-auto max-w-7xl py-2 sm:px-6 lg:px-8">
-            <div className="mx-auto max-w-screen-xl bg-white p-5 sm:p-10 md:p-16">
+            <div className="mx-auto mb-14 max-w-screen-xl bg-white p-5 sm:p-10 md:p-16">
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router";
 
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 w-full bg-pink-50 p-2">
-      <div className="flex flex-row items-center justify-center gap-4">
+    <footer className="fixed bottom-0 w-full bg-pink-50 px-5 py-2 md:p-2">
+      <div className="flex flex-row items-center justify-center gap-1 md:gap-4">
         <div className="border-r-2 border-gray-200">
           <Link to="/" className="flex flex-shrink-0 items-center pr-3">
             <img
