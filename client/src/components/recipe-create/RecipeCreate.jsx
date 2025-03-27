8dac6c9d6bc34b30f552db0fba794a7d7ee9b757
Luiza Petrinina <petrinina95@gmail.com>
2025-03-27 11:20:43 +0200

    Change navigate path

--- client/src/components/recipe-create/RecipeCreate.jsx
+++ client/src/components/recipe-create/RecipeCreate.jsx
@@ -15,7 +15,7 @@ export default function RecipeCreate() {
 
     await create(recipeData);
 
-    navigate("/");
+    navigate("/recipes");
   };
 
   const [state, submitionAction, isPending] = useActionState(submitHandler, {
